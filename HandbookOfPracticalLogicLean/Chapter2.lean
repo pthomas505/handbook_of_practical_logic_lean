5a3cdd4cee515bc1f7ae4270f3897cb5ec1397b4
Patrick Thomas <pthomas505@gmail.com>
2025-01-25 22:15:57 -0800

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2.lean
+++ HandbookOfPracticalLogicLean/Chapter2.lean
@@ -1108,6 +1108,61 @@ theorem corollary_2_8_b
   simp
 
 
+def is_subformula
+  (F : Formula_) :
+  Formula_ → Prop
+  | false_ => F = false_
+  | true_ => F = true_
+  | atom_ X => F = atom_ X
+  | not_ phi =>
+    F = not_ phi ∨
+    is_subformula F phi
+  | and_ phi psi =>
+    F = and_ phi psi ∨
+    is_subformula F phi ∨
+    is_subformula F psi
+  | or_ phi psi =>
+    F = or_ phi psi ∨
+    is_subformula F phi ∨
+    is_subformula F psi
+  | imp_ phi psi =>
+    F = imp_ phi psi ∨
+    is_subformula F phi ∨
+    is_subformula F psi
+  | iff_ phi psi =>
+    F = iff_ phi psi ∨
+    is_subformula F phi ∨
+    is_subformula F psi
+  | forall_ x phi =>
+    F = forall_ x phi ∨
+    is_subformula F phi
+  | exists_ x phi =>
+    F = exists_ x phi ∨
+    is_subformula F phi
+
+instance
+  (F F' : Formula_) :
+  Decidable (is_subformula F F') :=
+  by
+  induction F'
+  all_goals
+    simp only [is_subformula]
+    infer_instance
+
+
+def is_proper_subformula
+  (F F' : Formula_) :
+  Prop :=
+  is_subformula F F' ∧ ¬ F = F'
+
+instance
+  (F F' : Formula_) :
+  Decidable (is_proper_subformula F F') :=
+  by
+  unfold is_proper_subformula
+  infer_instance
+
+
 def simplify_aux :
   Formula_ → Formula_
   | not_ false_ => true_
