3f58c70af175009c8fca17e55095634f5dba15cd
Patrick Thomas <pthomas505@gmail.com>
2025-01-26 21:08:44 -0800

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2.lean
+++ HandbookOfPracticalLogicLean/Chapter2.lean
@@ -1149,6 +1149,17 @@ instance
     infer_instance
 
 
+lemma not_is_subformula_imp_not_equal
+  (F F' : Formula_)
+  (h1 : ¬ is_subformula F F') :
+  ¬ F' = F :=
+  by
+  cases F'
+  all_goals
+    simp only [is_subformula] at h1
+    tauto
+
+
 def is_proper_subformula
   (F F' : Formula_) :
   Prop :=
@@ -1184,6 +1195,105 @@ def simplify_aux :
   | iff_ phi false_ => not_ phi
   | iff_ false_ phi => not_ phi
   | phi => phi
+
+
+def simplify_aux_and :
+  Formula_ → Formula_
+  | and_ _ false_ => false_
+  | and_ false_ _ => false_
+  | and_ phi true_ => phi
+  | and_ true_ phi => phi
+  | phi => phi
+
+
+example
+  (F : Formula_) :
+  simplify_aux_and (and_ F false_) = false_ :=
+  by
+  simp only [simplify_aux_and]
+
+
+example
+  (F : Formula_) :
+  simplify_aux_and (and_ false_ F) = false_ :=
+  by
+  unfold simplify_aux_and
+  split
+  case _ phi psi h1 =>
+    rfl
+  case _ phi psi h1 h2 =>
+    rfl
+  case _ phi psi h1 h2 =>
+    cases h2
+    rfl
+  case _ phi psi h1 h2 h3 =>
+    cases h3
+  · tauto
+
+
+example
+  (F : Formula_) :
+  simplify_aux_and (and_ true_ F) = F :=
+  by
+  unfold simplify_aux_and
+  split
+  case _ phi psi h1 =>
+    cases h1
+    rfl
+  case _ phi psi h1 h2 =>
+    cases h2
+  case _ phi psi h1 h2 =>
+    cases h2
+    rfl
+  case _ phi psi h1 h2 h3 =>
+    cases h3
+    rfl
+  · tauto
+
+
+example
+  (F : Formula_) :
+  simplify_aux_and (and_ F true_) = F :=
+  by
+  unfold simplify_aux_and
+  split
+  case _ phi psi h1 =>
+    cases h1
+  case _ phi psi h1 h2 =>
+    cases h2
+    rfl
+  case _ phi psi h1 h2 =>
+    cases h2
+    rfl
+  case _ phi psi h1 h2 h3 =>
+    cases h3
+    rfl
+  · tauto
+
+
+example
+  (P Q : Formula_)
+  (h1 : ¬ P = false_)
+  (h2 : ¬ P = true_)
+  (h3 : ¬ Q = false_)
+  (h4 : ¬ Q = true_) :
+  simplify_aux_and (and_ P Q) = and_ P Q :=
+  by
+  unfold simplify_aux_and
+  split
+  case _ phi psi ih_1 =>
+    cases ih_1
+    contradiction
+  case _ phi psi ih_1 ih_2 =>
+    cases ih_2
+    contradiction
+  case _ phi psi ih_1 ih_2 =>
+    cases ih_2
+    contradiction
+  case _ phi psi ih_1 ih_2 ih_3 =>
+    cases ih_3
+    contradiction
+  · tauto
 
 
 lemma simplify_aux_is_logically_equivalent
@@ -1709,3 +1819,181 @@ example
 
 
 -------------------------------------------------------------------------------
+
+/-
+example
+  (F : Formula_)
+  (h1 : F.is_prop) :
+  ¬ is_proper_subformula false_ (simplify F) ∧
+  ¬ is_proper_subformula true_ (simplify F) :=
+  by
+  induction F
+  case and_ phi psi phi_ih psi_ih =>
+    simp only [is_prop] at h1
+    obtain ⟨h1_left, h1_right⟩ := h1
+
+    simp only [simplify]
+    specialize phi_ih h1_left
+    specialize psi_ih h1_right
+
+    simp only [is_proper_subformula] at phi_ih
+
+    have s1 : simplify phi = true_ ∨ ¬ is_subformula true_ (simplify phi) :=
+    by
+      tauto
+
+    have s2 : simplify phi = false_ ∨ ¬ is_subformula false_ (simplify phi) :=
+    by
+      tauto
+
+    clear phi_ih
+
+    simp only [is_proper_subformula] at psi_ih
+
+    have s3 : simplify psi = true_ ∨ ¬ is_subformula true_ (simplify psi) :=
+    by
+      tauto
+
+    have s4 : simplify psi = false_ ∨ ¬ is_subformula false_ (simplify psi) :=
+    by
+      tauto
+
+    clear psi_ih
+
+    constructor
+    · cases s1
+      case _ s1 =>
+        cases s2
+        case _ s2 =>
+          rewrite [s1] at s2
+          contradiction
+        case _ s2 =>
+          rewrite [s1]
+          cases s3
+          case _ s3 =>
+            rewrite [s3]
+            simp only [simplify_aux]
+            simp only [is_proper_subformula]
+            simp only [is_subformula]
+            tauto
+          case _ s3 =>
+            cases s4
+            case _ s4 =>
+              rewrite [s4]
+              simp only [simplify_aux]
+              simp only [is_proper_subformula]
+              simp only [is_subformula]
+              tauto
+            case _ s4 =>
+              have s5 : ¬ simplify psi = true_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s3
+
+              have s6 : ¬ simplify psi = false_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s4
+
+              simp only [simplify_aux]
+              simp only [is_proper_subformula]
+              tauto
+      case _ s1 =>
+        cases s2
+        case _ s2 =>
+          rewrite [s2]
+          cases s3
+          case _ s3 =>
+            rewrite [s3]
+            simp only [simplify_aux]
+            simp only [is_proper_subformula]
+            tauto
+          case _ s3 =>
+            cases s4
+            case _ s4 =>
+              rewrite [s4]
+              simp only [simplify_aux]
+              simp only [is_proper_subformula]
+              tauto
+            case _ s4 =>
+              have s5 : ¬ simplify psi = true_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s3
+
+              have s6 : ¬ simplify psi = false_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s4
+
+              simp only [simplify_aux]
+              simp only [is_proper_subformula]
+              tauto
+        case _ s2 =>
+          cases s3
+          case _ s3 =>
+            rewrite [s3]
+            cases s4
+            case _ s4 =>
+              rewrite [s3] at s4
+              contradiction
+            case _ s4 =>
+              have s5 : ¬ simplify phi = true_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s1
+
+              have s6 : ¬ simplify phi = false_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s2
+
+              simp only [simplify_aux]
+              simp only [is_proper_subformula]
+              tauto
+          case _ s3 =>
+            cases s4
+            case _ s4 =>
+              rewrite [s4]
+
+              have s5 : ¬ simplify phi = true_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s1
+
+              have s6 : ¬ simplify phi = false_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s2
+
+              simp only [simplify_aux]
+              simp only [is_proper_subformula]
+              tauto
+            case _ s4 =>
+              have s5 : ¬ simplify phi = true_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s1
+
+              have s6 : ¬ simplify phi = false_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s2
+
+              have s7 : ¬ simplify psi = true_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s3
+
+              have s8 : ¬ simplify psi = false_ :=
+              by
+                apply not_is_subformula_imp_not_equal
+                exact s4
+
+              simp only [simplify_aux]
+              simp_all only [and_.injEq, and_false, implies_true, false_and]
+              simp only [is_proper_subformula]
+              simp only [is_subformula]
+              tauto
+    · sorry
+-/
