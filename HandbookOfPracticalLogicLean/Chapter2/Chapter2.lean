edc66b0f7e126389fed691af4f1526ed3a4eb3d5
Patrick Thomas <pthomas505@gmail.com>
2025-01-28 20:35:34 -0800

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/Chapter2.lean
+++ HandbookOfPracticalLogicLean/Chapter2/Chapter2.lean
@@ -14,6 +14,18 @@ namespace Prop_
 open Formula_
 
 
+def Formula_.atom_set :
+  Formula_ → Finset String
+  | false_ => ∅
+  | true_ => ∅
+  | atom_ X => {X}
+  | not_ phi => phi.atom_set
+  | and_ phi psi => phi.atom_set ∪ psi.atom_set
+  | or_ phi psi => phi.atom_set ∪ psi.atom_set
+  | imp_ phi psi => phi.atom_set ∪ psi.atom_set
+  | iff_ phi psi => phi.atom_set ∪ psi.atom_set
+
+
 def atom_occurs_in
   (A : String) :
   Formula_ → Prop
@@ -25,6 +37,32 @@ def atom_occurs_in
   | or_ phi psi => atom_occurs_in A phi ∨ atom_occurs_in A psi
   | imp_ phi psi => atom_occurs_in A phi ∨ atom_occurs_in A psi
   | iff_ phi psi => atom_occurs_in A phi ∨ atom_occurs_in A psi
+
+
+example
+  (A : String)
+  (F : Formula_) :
+  atom_occurs_in A F ↔ A ∈ F.atom_set :=
+  by
+  induction F
+  all_goals
+    unfold atom_occurs_in
+    unfold atom_set
+  case true_ | false_ =>
+    simp
+  case atom_ X =>
+    simp
+  case not_ phi ih =>
+    exact ih
+  case
+      and_ phi psi phi_ih psi_ih
+    | or_ phi psi phi_ih psi_ih
+    | imp_ phi psi phi_ih psi_ih
+    | iff_ phi psi phi_ih psi_ih =>
+    simp
+    rewrite [phi_ih]
+    rewrite [psi_ih]
+    rfl
 
 
 theorem theorem_2_2
