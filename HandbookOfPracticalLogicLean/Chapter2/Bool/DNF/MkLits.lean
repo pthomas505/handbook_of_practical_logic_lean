2a5c4a1c7215eab4498ccb95b7ef60cf5bd067f9
Patrick Thomas <pthomas505@gmail.com>
2025-03-22 19:20:19 -0700

    Updates.

---
+++ HandbookOfPracticalLogicLean/Chapter2/Bool/DNF/MkLits.lean
@@ -0,0 +1,15 @@
+import MathlibExtraLean.FunctionUpdateFromListOfPairsITE
+
+import HandbookOfPracticalLogicLean.Chapter2.Bool.DNF.IsDNF
+import HandbookOfPracticalLogicLean.Chapter2.Bool.DNF.ListConj
+
+import Mathlib.Tactic
+
+
+set_option autoImplicit false
+
+
+namespace Bool_
+
+
+open Formula_
