9690c0826120da6d764c5e5de9f4ea633c1ae287
Patrick Thomas <pthomas505@gmail.com>
2025-03-22 19:44:53 -0700

    Updates.

---
+++ HandbookOfPracticalLogicLean/Chapter2/Bool/DNF/GenAllValuations.lean
@@ -0,0 +1,17 @@
+import MathlibExtraLean.FunctionUpdateFromListOfPairsITE
+
+import HandbookOfPracticalLogicLean.Chapter2.Bool.DNF.IsDNF
+import HandbookOfPracticalLogicLean.Chapter2.Bool.DNF.ListConj
+import HandbookOfPracticalLogicLean.Chapter2.Bool.DNF.MkLits
+import HandbookOfPracticalLogicLean.Chapter2.Bool.DNF.ListDisj
+
+import Mathlib.Tactic
+
+
+set_option autoImplicit false
+
+
+namespace Bool_
+
+
+open Formula_
