fcd1b7b3e233dfdec68b34c2f4bf4267126cf785
Patrick Thomas <pthomas505@gmail.com>
2025-05-03 19:23:11 -0700

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/CNF/DefCNF.lean
+++ HandbookOfPracticalLogicLean/Chapter2/CNF/DefCNF.lean
@@ -53,36 +53,55 @@ theorem theorem_2_10
     exact a1_right
 
 
-def mk_defs
+-- https://avigad.github.io/lamr/decision_procedures_for_propositional_logic.html#the-tseitin-transformation
+
+def mk_defs_aux
   (acc : Array Formula_) :
   Formula_ → (Formula_ × Array Formula_)
   | false_ => (false_, acc)
   | true_ => (true_, acc)
-  | atom_ X => (atom_ X, acc)
+  | atom_ X =>
+      (atom_ X, acc)
 /-
       match acc.findIdx? (· = (atom_ X)) with
-      | some n => (atom_ n.repr, acc)
-      | none => (atom_ acc.size.repr, acc.push (atom_ X))
+      | some n => (atom_ s!"def_{n}", acc)
+      | none => (atom_ s!"def_{acc.size}", acc.push (atom_ X))
 -/
-  | not_ (atom_ X) => (not_ (atom_ X), acc)
+  | not_ (atom_ X) =>
+      (not_ (atom_ X), acc)
 /-
       match acc.findIdx? (· = (atom_ X)) with
-      | some n => (not_ (atom_ n.repr), acc)
-      | none => (not_ (atom_ acc.size.repr), acc.push (atom_ X))
+      | some n => (not_ (atom_ s!"def_{n}"), acc)
+      | none => (not_ (atom_ s!"def_{acc.size}"), acc.push (atom_ X))
 -/
   | and_ phi psi =>
-      let (phi_def, phi_acc) := mk_defs acc phi
-      let (psi_def, psi_acc) := mk_defs phi_acc psi
+      let (phi_def, phi_acc) := mk_defs_aux acc phi
+      let (psi_def, psi_acc) := mk_defs_aux phi_acc psi
       match psi_acc.findIdx? (· = and_ phi_def psi_def) with
-      | some n => (atom_ n.repr, psi_acc)
-      | none => (atom_ psi_acc.size.repr, psi_acc.push (and_ phi_def psi_def))
+      | some n => (atom_ s!"def_{n}", psi_acc)
+      | none => (atom_ s!"def_{psi_acc.size.repr}", psi_acc.push (and_ phi_def psi_def))
   | or_ phi psi =>
-      let (phi_def, phi_acc) := mk_defs acc phi
-      let (psi_def, psi_acc) := mk_defs phi_acc psi
+      let (phi_def, phi_acc) := mk_defs_aux acc phi
+      let (psi_def, psi_acc) := mk_defs_aux phi_acc psi
       match psi_acc.findIdx? (· = or_ phi_def psi_def) with
-      | some n => (atom_ n.repr, psi_acc)
-      | none => (atom_ psi_acc.size.repr, psi_acc.push (or_ phi_def psi_def))
+      | some n => (atom_ s!"def_{n}", psi_acc)
+      | none => (atom_ s!"def_{psi_acc.size.repr}", psi_acc.push (or_ phi_def psi_def))
   | F => (F, acc)
 
 
-#eval (mk_defs #[] (Formula_| ((((p /\ q) /\ ~ r) \/ (r /\ (~ p \/ ~ q))) /\ (~ s \/ (p /\ t))))).snd
+def mk_defs
+  (F : Formula_) :
+  (Formula_ × Array Formula_) :=
+  mk_defs_aux #[] F
+
+
+def print_defs
+  (F : Formula_) :
+  IO Unit := do
+  let ⟨last, defs⟩ := mk_defs F
+  IO.println s!"{last}, where"
+  for i in [:defs.size] do
+    IO.println s!"def_{i} := {defs[i]!}"
+
+
+#eval print_defs (Formula_| ((((p /\ q) /\ ~ r) \/ (r /\ (~ p \/ ~ q))) /\ (~ s \/ (p /\ t))))
