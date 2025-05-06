eb1a2ac08474f38ae5b43d21eef33aa02776df80
Patrick Thomas <pthomas505@gmail.com>
2025-05-05 21:53:36 -0700

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/NF.lean
+++ HandbookOfPracticalLogicLean/Chapter2/NF.lean
@@ -589,3 +589,252 @@ lemma is_literal_ind_iff_is_literal_rec
 
 
 -------------------------------------------------------------------------------
+
+
+lemma is_conj_rec_v1_imp_is_nnf_rec_v1
+  (F : Formula_)
+  (h1 : F.is_conj_rec_v1) :
+  F.is_nnf_rec_v1 :=
+  by
+  induction F
+  case false_ | true_ | atom_ X =>
+    unfold is_nnf_rec_v1
+    exact trivial
+  case not_ phi ih =>
+    cases phi
+    case atom_ X =>
+      unfold is_nnf_rec_v1
+      exact trivial
+    all_goals
+      unfold is_conj_rec_v1 at h1
+      contradiction
+  case and_ phi psi phi_ih psi_ih =>
+    unfold is_nnf_rec_v1
+    cases phi
+    case false_ | true_ | atom_ X =>
+      unfold is_conj_rec_v1 at h1
+      constructor
+      · unfold is_nnf_rec_v1
+        exact trivial
+      · apply psi_ih
+        exact h1
+    case not_ phi =>
+      cases phi
+      case atom_ X =>
+        unfold is_conj_rec_v1 at h1
+        constructor
+        · unfold is_nnf_rec_v1
+          exact trivial
+        · apply psi_ih
+          exact h1
+      all_goals
+        unfold is_conj_rec_v1 at h1
+        contradiction
+    all_goals
+      unfold is_conj_rec_v1 at h1
+      contradiction
+  all_goals
+    unfold is_conj_rec_v1 at h1
+    contradiction
+
+
+lemma is_conj_rec_v1_imp_is_conj_ind_v1
+  (F : Formula_)
+  (h1 : is_conj_rec_v1 F) :
+  is_conj_ind_v1 F :=
+  by
+  induction F
+  case false_ =>
+    apply is_conj_ind_v1.rule_3
+    apply is_constant_ind.rule_1
+  case true_ =>
+    apply is_conj_ind_v1.rule_3
+    apply is_constant_ind.rule_2
+  case atom_ X =>
+    apply is_conj_ind_v1.rule_4
+    apply is_literal_ind.rule_1
+  case not_ phi ih =>
+    cases phi
+    case atom_ X =>
+      apply is_conj_ind_v1.rule_4
+      apply is_literal_ind.rule_2
+    all_goals
+      simp only [is_conj_rec_v1] at h1
+  case and_ phi psi phi_ih psi_ih =>
+    cases phi
+    case false_ =>
+      simp only [is_conj_rec_v1] at h1
+      apply is_conj_ind_v1.rule_1
+      · apply is_constant_ind.rule_1
+      · apply psi_ih
+        exact h1
+    case true_ =>
+      simp only [is_conj_rec_v1] at h1
+      apply is_conj_ind_v1.rule_1
+      · apply is_constant_ind.rule_2
+      · apply psi_ih
+        exact h1
+    case atom_ X =>
+      simp only [is_conj_rec_v1] at h1
+      apply is_conj_ind_v1.rule_2
+      · apply is_literal_ind.rule_1
+      · apply psi_ih
+        exact h1
+    case not_ phi =>
+      cases phi
+      case atom_ X =>
+        simp only [is_conj_rec_v1] at h1
+        simp only [is_conj_rec_v1] at phi_ih
+        apply is_conj_ind_v1.rule_2
+        · apply is_literal_ind.rule_2
+        · apply psi_ih
+          exact h1
+      all_goals
+        simp only [is_conj_rec_v1] at h1
+    all_goals
+      simp only [is_conj_rec_v1] at h1
+  all_goals
+    simp only [is_conj_rec_v1] at h1
+
+
+lemma is_conj_ind_v1_imp_is_conj_rec_v1
+  (F : Formula_)
+  (h1 : is_conj_ind_v1 F) :
+  is_conj_rec_v1 F :=
+  by
+  induction h1
+  case rule_1 phi psi ih_1 ih_2 ih_3 =>
+    cases ih_1
+    case rule_1 =>
+      simp only [is_conj_rec_v1]
+      exact ih_3
+    case rule_2 =>
+      simp only [is_conj_rec_v1]
+      exact ih_3
+  case rule_2 phi psi ih_1 ih_2 ih_3 =>
+    cases ih_1
+    case rule_1 X =>
+      simp only [is_conj_rec_v1]
+      exact ih_3
+    case rule_2 X =>
+      simp only [is_conj_rec_v1]
+      exact ih_3
+  case rule_3 phi ih_1 =>
+    cases ih_1
+    case rule_1 =>
+      simp only [is_conj_rec_v1]
+    case rule_2 =>
+      simp only [is_conj_rec_v1]
+  case rule_4 phi ih_1 =>
+    cases ih_1
+    case rule_1 X =>
+      simp only [is_conj_rec_v1]
+    case rule_2 X =>
+      simp only [is_conj_rec_v1]
+
+
+lemma is_conj_rec_v1_iff_is_conj_ind_v1
+  (F : Formula_) :
+  is_conj_rec_v1 F ↔ is_conj_ind_v1 F :=
+  by
+  constructor
+  · apply is_conj_rec_v1_imp_is_conj_ind_v1
+  · apply is_conj_ind_v1_imp_is_conj_rec_v1
+
+
+-------------------------------------------------------------------------------
+
+
+lemma is_dnf_rec_v1_imp_is_dnf_ind_v1
+  (F : Formula_)
+  (h1 : is_dnf_rec_v1 F) :
+  is_dnf_ind_v1 F :=
+  by
+  induction F
+  case false_ =>
+    apply is_dnf_ind_v1.rule_2
+    apply is_conj_ind_v1.rule_3
+    apply is_constant_ind.rule_1
+  case true_ =>
+    apply is_dnf_ind_v1.rule_2
+    apply is_conj_ind_v1.rule_3
+    apply is_constant_ind.rule_2
+  case atom_ X =>
+    apply is_dnf_ind_v1.rule_2
+    apply is_conj_ind_v1.rule_4
+    apply is_literal_ind.rule_1
+  case not_ phi ih =>
+    cases phi
+    case atom_ X =>
+      apply is_dnf_ind_v1.rule_2
+      apply is_conj_ind_v1.rule_4
+      apply is_literal_ind.rule_2
+    all_goals
+      simp only [is_dnf_rec_v1] at h1
+      simp only [is_conj_rec_v1] at h1
+  case or_ phi psi phi_ih psi_ih =>
+    unfold is_dnf_rec_v1 at h1
+    obtain ⟨h1_left, h1_right⟩ := h1
+
+    apply is_dnf_ind_v1.rule_1
+    · apply is_conj_rec_v1_imp_is_conj_ind_v1
+      exact h1_left
+    · apply psi_ih
+      exact h1_right
+  case and_ phi psi phi_ih psi_ih =>
+    unfold is_dnf_rec_v1 at h1
+    apply is_dnf_ind_v1.rule_2
+    apply is_conj_rec_v1_imp_is_conj_ind_v1
+    exact h1
+  all_goals
+    simp only [is_dnf_rec_v1] at h1
+    simp only [is_conj_rec_v1] at h1
+
+
+lemma is_dnf_ind_v1_imp_is_dnf_rec_v1
+  (F : Formula_)
+  (h1 : is_dnf_ind_v1 F) :
+  is_dnf_rec_v1 F :=
+  by
+  induction h1
+  case rule_1 phi psi ih_1 ih_2 ih_3 =>
+    unfold is_dnf_rec_v1
+    constructor
+    · apply is_conj_ind_v1_imp_is_conj_rec_v1
+      exact ih_1
+    · exact ih_3
+  case rule_2 phi ih_1 =>
+    cases ih_1
+    case rule_1 phi psi phi_ih psi_ih =>
+      unfold is_dnf_rec_v1
+      apply is_conj_ind_v1_imp_is_conj_rec_v1
+      apply is_conj_ind_v1.rule_1
+      · exact phi_ih
+      · exact psi_ih
+    case rule_2 phi psi phi_ih psi_ih =>
+      unfold is_dnf_rec_v1
+      apply is_conj_ind_v1_imp_is_conj_rec_v1
+      apply is_conj_ind_v1.rule_2
+      · exact phi_ih
+      · exact psi_ih
+    case rule_3 ih =>
+      cases ih
+      all_goals
+        unfold is_dnf_rec_v1
+        unfold is_conj_rec_v1
+        exact trivial
+    case rule_4 ih =>
+      cases ih
+      all_goals
+        unfold is_dnf_rec_v1
+        unfold is_conj_rec_v1
+        exact trivial
+
+
+lemma is_dnf_rec_v1_iff_is_dnf_ind_v1
+  (F : Formula_) :
+  is_dnf_rec_v1 F ↔ is_dnf_ind_v1 F :=
+  by
+  constructor
+  · apply is_dnf_rec_v1_imp_is_dnf_ind_v1
+  · apply is_dnf_ind_v1_imp_is_dnf_rec_v1
