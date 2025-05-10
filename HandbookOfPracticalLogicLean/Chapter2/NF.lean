b875a5509f8e5d8df05ecb61e91cf85d3f66c811
Patrick Thomas <pthomas505@gmail.com>
2025-05-09 22:17:48 -0700

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/NF.lean
+++ HandbookOfPracticalLogicLean/Chapter2/NF.lean
@@ -1207,6 +1207,11 @@ lemma is_dnf_rec_v1_imp_is_dnf_ind_v1
     all_goals
       simp only [is_dnf_rec_v1] at h1
       simp only [is_conj_rec_v1] at h1
+  case and_ phi psi phi_ih psi_ih =>
+    unfold is_dnf_rec_v1 at h1
+    apply is_dnf_ind_v1.rule_2
+    apply is_conj_rec_v1_imp_is_conj_ind_v1
+    exact h1
   case or_ phi psi phi_ih psi_ih =>
     unfold is_dnf_rec_v1 at h1
     obtain ⟨h1_left, h1_right⟩ := h1
@@ -1216,11 +1221,6 @@ lemma is_dnf_rec_v1_imp_is_dnf_ind_v1
       exact h1_left
     · apply psi_ih
       exact h1_right
-  case and_ phi psi phi_ih psi_ih =>
-    unfold is_dnf_rec_v1 at h1
-    apply is_dnf_ind_v1.rule_2
-    apply is_conj_rec_v1_imp_is_conj_ind_v1
-    exact h1
   all_goals
     simp only [is_dnf_rec_v1] at h1
     simp only [is_conj_rec_v1] at h1
@@ -1273,6 +1273,95 @@ lemma is_dnf_rec_v1_iff_is_dnf_ind_v1
   constructor
   · apply is_dnf_rec_v1_imp_is_dnf_ind_v1
   · apply is_dnf_ind_v1_imp_is_dnf_rec_v1
+
+
+-------------------------------------------------------------------------------
+
+
+lemma is_dnf_rec_v2_imp_is_dnf_ind_v2
+  (F : Formula_)
+  (h1 : is_dnf_rec_v2 F) :
+  is_dnf_ind_v2 F :=
+  by
+  induction F
+  case false_ =>
+    apply is_dnf_ind_v2.rule_2
+    apply is_conj_ind_v2.rule_2
+    apply is_constant_ind.rule_1
+  case true_ =>
+    apply is_dnf_ind_v2.rule_2
+    apply is_conj_ind_v2.rule_2
+    apply is_constant_ind.rule_2
+  case atom_ X =>
+    apply is_dnf_ind_v2.rule_2
+    apply is_conj_ind_v2.rule_3
+    apply is_literal_ind.rule_1
+  case not_ phi ih =>
+    cases phi
+    case atom_ X =>
+      apply is_dnf_ind_v2.rule_2
+      apply is_conj_ind_v2.rule_3
+      apply is_literal_ind.rule_2
+    all_goals
+      simp only [is_dnf_rec_v2] at h1
+      simp only [is_conj_rec_v2] at h1
+  case and_ phi psi phi_ih psi_ih =>
+    unfold is_dnf_rec_v2 at h1
+    apply is_dnf_ind_v2.rule_2
+    apply is_conj_rec_v2_imp_is_conj_ind_v2
+    exact h1
+  case or_ phi psi phi_ih psi_ih =>
+    unfold is_dnf_rec_v2 at h1
+    obtain ⟨h1_left, h1_right⟩ := h1
+
+    apply is_dnf_ind_v2.rule_1
+    · apply phi_ih
+      exact h1_left
+    · apply psi_ih
+      exact h1_right
+  all_goals
+    simp only [is_dnf_rec_v2] at h1
+    simp only [is_conj_rec_v2] at h1
+
+
+lemma is_dnf_ind_v2_imp_is_dnf_rec_v2
+  (F : Formula_)
+  (h1 : is_dnf_ind_v2 F) :
+  is_dnf_rec_v2 F :=
+  by
+  induction h1
+  case rule_1 phi psi ih_1 ih_2 ih_3 ih_4 =>
+    unfold is_dnf_rec_v2
+    exact ⟨ih_3, ih_4⟩
+  case rule_2 phi ih_1 =>
+    cases ih_1
+    case rule_1 phi psi phi_ih psi_ih =>
+      unfold is_dnf_rec_v2
+      apply is_conj_ind_v2_imp_is_conj_rec_v2
+      apply is_conj_ind_v2.rule_1
+      · exact phi_ih
+      · exact psi_ih
+    case rule_2 ih =>
+      cases ih
+      all_goals
+        unfold is_dnf_rec_v2
+        unfold is_conj_rec_v2
+        exact trivial
+    case rule_3 ih =>
+      cases ih
+      all_goals
+        unfold is_dnf_rec_v2
+        unfold is_conj_rec_v2
+        exact trivial
+
+
+lemma is_dnf_rec_v2_iff_is_dnf_ind_v2
+  (F : Formula_) :
+  is_dnf_rec_v2 F ↔ is_dnf_ind_v2 F :=
+  by
+  constructor
+  · apply is_dnf_rec_v2_imp_is_dnf_ind_v2
+  · apply is_dnf_ind_v2_imp_is_dnf_rec_v2
 
 
 -------------------------------------------------------------------------------
