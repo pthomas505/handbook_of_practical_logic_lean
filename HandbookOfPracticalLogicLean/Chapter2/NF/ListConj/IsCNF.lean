b4619217ad6820eb01f78a36257a2cff4294ba77
Patrick Thomas <pthomas505@gmail.com>
2025-05-17 09:34:07 -0700

    Updates.

---
+++ HandbookOfPracticalLogicLean/Chapter2/NF/ListConj/IsCNF.lean
@@ -0,0 +1,8 @@
+import HandbookOfPracticalLogicLean.Chapter2.NF.ListConj.ListConj
+import HandbookOfPracticalLogicLean.Chapter2.NF.NF
+
+
+set_option autoImplicit false
+
+
+open Formula_
---
+++ HandbookOfPracticalLogicLean/Chapter2/NF/ListConj/Semantics.lean
@@ -0,0 +1,9 @@
+import HandbookOfPracticalLogicLean.Chapter2.Semantics
+
+import HandbookOfPracticalLogicLean.Chapter2.NF.ListConj.ListConj
+
+
+set_option autoImplicit false
+
+
+open Formula_
