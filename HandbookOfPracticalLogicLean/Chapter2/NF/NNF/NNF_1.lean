5bf8af54c6fc7a4a4204ab18e54fd0aded174f38
Patrick Thomas <pthomas505@gmail.com>
2025-05-16 19:36:03 -0700

    Updates.

---
+++ HandbookOfPracticalLogicLean/Chapter2/NF/NNF/NNF_1.lean
@@ -0,0 +1,14 @@
+import HandbookOfPracticalLogicLean.Chapter2.Replace
+import HandbookOfPracticalLogicLean.Chapter2.Semantics
+import HandbookOfPracticalLogicLean.Chapter2.SubFormula
+
+import HandbookOfPracticalLogicLean.Chapter2.NF.NF
+
+
+import Mathlib.Tactic
+
+
+set_option autoImplicit false
+
+
+open Formula_
