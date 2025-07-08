2a5a6ba4bf7500ec5058c458a1b2d0d7a1b0d5c6
Patrick Thomas <pthomas505@gmail.com>
2025-07-07 20:30:47 -0700

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/Size.lean
+++ HandbookOfPracticalLogicLean/Chapter2/Size.lean
@@ -1,4 +1,4 @@
-import HandbookOfPracticalLogicLean.Chapter2.SubFormula
+import HandbookOfPracticalLogicLean.Chapter2.Formula
 
 import Mathlib.Tactic
 
@@ -22,31 +22,6 @@ def Formula_.size :
   | or_ phi psi => phi.size + psi.size + 1
   | imp_ phi psi => phi.size + psi.size + 1
   | iff_ phi psi => phi.size + psi.size + 1
-
-
-example
-  (F : Formula_) :
-  size F = (subformula_list F).length :=
-  by
-  induction F
-  all_goals
-    unfold size
-    unfold subformula_list
-  case false_ | true_ | atom_ X =>
-    simp only [List.length_singleton]
-  case not_ phi ih =>
-    simp only [List.singleton_append, List.length_cons]
-    rewrite [ih]
-    rfl
-  case
-      and_ phi psi phi_ih psi_ih
-    | or_ phi psi phi_ih psi_ih
-    | imp_ phi psi phi_ih psi_ih
-    | iff_ phi psi phi_ih psi_ih =>
-    simp only [List.singleton_append, List.length_cons, List.length_append]
-    rewrite [phi_ih]
-    rewrite [psi_ih]
-    rfl
 
 
 #lint
--- HandbookOfPracticalLogicLean/Chapter2/SubFormula.lean
+++ HandbookOfPracticalLogicLean/Chapter2/SubFormula.lean
@@ -1,4 +1,4 @@
-import HandbookOfPracticalLogicLean.Chapter2.Formula
+import HandbookOfPracticalLogicLean.Chapter2.Size
 
 import Mathlib.Tactic
 
@@ -83,6 +83,31 @@ lemma is_subformula_iff_mem_subformula_list
     | imp_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
     simp only [List.mem_append, List.mem_singleton]
+    rewrite [phi_ih]
+    rewrite [psi_ih]
+    rfl
+
+
+example
+  (F : Formula_) :
+  size F = (subformula_list F).length :=
+  by
+  induction F
+  all_goals
+    unfold size
+    unfold subformula_list
+  case false_ | true_ | atom_ X =>
+    simp only [List.length_singleton]
+  case not_ phi ih =>
+    simp only [List.singleton_append, List.length_cons]
+    rewrite [ih]
+    rfl
+  case
+      and_ phi psi phi_ih psi_ih
+    | or_ phi psi phi_ih psi_ih
+    | imp_ phi psi phi_ih psi_ih
+    | iff_ phi psi phi_ih psi_ih =>
+    simp only [List.singleton_append, List.length_cons, List.length_append]
     rewrite [phi_ih]
     rewrite [psi_ih]
     rfl
