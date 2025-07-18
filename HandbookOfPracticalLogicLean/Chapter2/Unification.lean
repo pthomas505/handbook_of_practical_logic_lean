f711f7bf722887bf3b3e86d427643a011f0c94e8
Patrick Thomas <pthomas505@gmail.com>
2025-07-18 12:44:28 -0700

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/Unification.lean
+++ HandbookOfPracticalLogicLean/Chapter2/Unification.lean
@@ -166,11 +166,7 @@ example
         simp only at a1
         unfold replace_atom_all_rec at a1
         specialize a1 trivial
-        first
-          | simp only [and_.injEq] at a1
-          | simp only [or_.injEq] at a1
-          | simp only [imp_.injEq] at a1
-          | simp only [iff_.injEq] at a1
+        simp only [and_.injEq] at a1
         obtain ⟨a1_left, a1_right⟩ := a1
 
         cases a2
@@ -186,11 +182,7 @@ example
         rewrite [a2]
         simp only
         unfold replace_atom_all_rec
-        first
-        | simp only [and_.injEq]
-        | simp only [or_.injEq]
-        | simp only [imp_.injEq]
-        | simp only [iff_.injEq]
+        simp only [and_.injEq]
         constructor
         · specialize a1 (phi, phi')
           simp only at a1
