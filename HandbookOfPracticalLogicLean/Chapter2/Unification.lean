1b02686d25c7ccc5813381a87939912e0e04afa9
Patrick Thomas <pthomas505@gmail.com>
2025-07-20 10:33:09 -0700

    Updates.

--- HandbookOfPracticalLogicLean/Chapter2/Unification.lean
+++ HandbookOfPracticalLogicLean/Chapter2/Unification.lean
@@ -678,28 +678,6 @@ theorem not_is_small_step_singleton_refl
 -------------------------------------------------------------------------------
 
 
-lemma is_small_step_append_left
-  (E_1 E_2 : List (String × Formula_))
-  (X Y : String)
-  (h1 : is_small_step_v1 (E_1 ++ E_2) X Y) :
-  (is_small_step_v1 E_1 X Y) ∨ (is_small_step_v1 E_2 X Y) :=
-  by
-  unfold is_small_step_v1 at h1
-  obtain ⟨F, h1_left, h1_right⟩ := h1
-  simp only [List.mem_append] at h1_left
-
-  unfold is_small_step_v1
-  cases h1_left
-  case inl h1_left =>
-    left
-    apply Exists.intro F
-    exact ⟨h1_left, h1_right⟩
-  case inr h1_left =>
-    right
-    apply Exists.intro F
-    exact ⟨h1_left, h1_right⟩
-
-
 lemma is_small_step_append_right
   (E_1 E_2 : List (String × Formula_))
   (X Y : String)
@@ -729,6 +707,28 @@ lemma is_small_step_append_right
       right
       exact h1_left
     · exact h1_right
+
+
+lemma is_small_step_append_left
+  (E_1 E_2 : List (String × Formula_))
+  (X Y : String)
+  (h1 : is_small_step_v1 (E_1 ++ E_2) X Y) :
+  (is_small_step_v1 E_1 X Y) ∨ (is_small_step_v1 E_2 X Y) :=
+  by
+  unfold is_small_step_v1 at h1
+  obtain ⟨F, h1_left, h1_right⟩ := h1
+  simp only [List.mem_append] at h1_left
+
+  unfold is_small_step_v1
+  cases h1_left
+  case inl h1_left =>
+    left
+    apply Exists.intro F
+    exact ⟨h1_left, h1_right⟩
+  case inr h1_left =>
+    right
+    apply Exists.intro F
+    exact ⟨h1_left, h1_right⟩
 
 
 example
